0255711ff227c9347899d917908d51fc200bf9af
xingfukun <874898417@qq.com>
2015-07-31 15:53:41 +0800

    init

---
+++ mycalayer/mycalayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mycalayer
+//
+//  Created by joymain on 15-7-31.
+//  Copyright (c) 2015年 邢富琨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mycalayer/mycalayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mycalayer
+//
+//  Created by joymain on 15-7-31.
+//  Copyright (c) 2015年 邢富琨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
